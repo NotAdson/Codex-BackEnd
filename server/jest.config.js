06ef499112b5d6532085800387ce628e59c9c3c2
Adson <adsongabrielfsilva@gmail.com>
2025-03-08 12:55:44 -0300

    feat: updating library and jest

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,8 +1,11 @@
 export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.js$": "babel-jest",
-  },
-  moduleFileExtensions: ["js", "json"],
-  testMatch: ["**/__tests__/**/*.test.js"],
+	testEnvironment: "node",
+	transform: {
+		"^.+\\.js$": "babel-jest",
+	},
+	moduleFileExtensions: ["js", "json"],
+	testMatch: ["**/__tests__/**/*.test.js"],
+	setupFiles: ["dotenv/config"],
+	globalSetup: './src/__tests__/jest.globalSetup.js',
+	globalTeardown: './src/__tests__/jest.globalTeardown.js'
 };
