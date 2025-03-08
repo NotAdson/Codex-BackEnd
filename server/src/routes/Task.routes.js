1cef4ccf87931dadca7498b75ab10822cce8e3e1
Adson <adsongabrielfsilva@gmail.com>
2025-03-08 12:58:46 -0300

    feat: creating task route

---
+++ server/src/routes/Task.routes.js
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import {
+	createTask,
+	getTasksByUser,
+	updateTask,
+	deleteTask,
+} from "../controllers/TaskController.js";
+import { TaskValidator } from "../middleware/Task.validator.js";
+import { verifyJWT } from "../authentication/Authenticator.js";
+
+const routerTask = Router();
+const instanceTaskValidator = new TaskValidator();
+
+routerTask.post(
+	"/task/create",
+	verifyJWT,
+	instanceTaskValidator.createTaskValidation,
+	async (req, res) => {
+		return await createTask(req, res);
+	}
+);
+
+routerTask.get(
+	"/tasks/:userId",
+	verifyJWT,
+	instanceTaskValidator.getTasksValidation,
+	async (req, res) => {
+		return await getTasksByUser(req, res);
+	}
+);
+
+routerTask.put(
+	"/task/update/:id",
+	verifyJWT,
+	instanceTaskValidator.updateTaskValidation,
+	async (req, res) => {
+		return await updateTask(req, res);
+	}
+);
+
+routerTask.delete(
+	"/task/delete/:id",
+	verifyJWT,
+	instanceTaskValidator.deleteTaskValidation,
+	async (req, res) => {
+		return await deleteTask(req, res);
+	}
+);
+
+export { routerTask };
